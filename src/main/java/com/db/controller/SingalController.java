4c881536a17356bb4b339b4fd58b5b7156928886
Lakshitha Ranasinghe <lakshitha@techventuras.com>
2023-08-10 00:18:12 +0200

    http layer implementation

--- src/main/java/com/db/controller/SingalController.java
+++ src/main/java/com/db/controller/SingalController.java
@@ -1,7 +1,24 @@
 package com.db.controller;
 
+import com.db.lib.SignalHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("signals")
 public class SingalController {
+    private final SignalHandler signalHandler;
+
+    @Autowired
+    public SingalController(SignalHandler signalHandler) {
+        this.signalHandler = signalHandler;
+    }
+
+    @PostMapping("{id}")
+    public void processSignal(@PathVariable int id){
+        this.signalHandler.handleSignal(id);
+    }
 }
