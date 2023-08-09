abb0bf79b7b6c23e7cc79990ba1417b45d65f749
Lakshitha Ranasinghe <lakshitha@techventuras.com>
2023-08-09 23:07:53 +0200

    enhance test suite

--- src/test/java/com/db/test/SignalHandlerServiceTest.java
+++ src/test/java/com/db/test/SignalHandlerServiceTest.java
@@ -2,7 +2,12 @@ package com.db.test;
 
 import com.db.client.JiraClient;
 import com.db.lib.SignalHandler;
+import com.db.model.SignalAlgoDetail;
+import com.db.model.SignalAlgoType;
 import com.db.model.SignalSpec;
+import com.db.model.algo.AlgoGeneric;
+import com.db.model.algo.AlgoParameterized;
+import com.db.model.algo.impl.*;
 import com.db.service.SignalHandlerImpl;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -12,7 +17,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -21,14 +28,29 @@ import static org.mockito.Mockito.when;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SignalHandlerServiceTest {
     private SignalHandler signalHandler;
-
     @Mock
     private JiraClient jiraClient = mock(JiraClient.class);
+    private Map<String, AlgoGeneric> algoGenericMap;
+    private Map<String, AlgoParameterized> algoParameterizedMap;
     @BeforeAll
     public void setUp(){
         signalHandler = new SignalHandlerImpl(jiraClient);
         when(jiraClient.fetchSignalSpecs()).thenReturn(generateSampleSignals());
     }
+
+    @BeforeAll
+    public void setupTemplate(){
+        algoGenericMap = new HashMap<>();
+        algoParameterizedMap = new HashMap<>();
+        algoGenericMap.put("do", new AlgoDo());
+        algoGenericMap.put("cancel", new AlgoCancel());
+        algoGenericMap.put("reverse", new AlgoReverse());
+        algoGenericMap.put("submitMarket", new AlgoSubmit());
+        algoGenericMap.put("performCalculation", new AlgoCalculate());
+        algoGenericMap.put("setup", new AlgoSetup());
+        algoParameterizedMap.put("setParam", new AlgoParam());
+    }
+
     @Test
     public void shouldAcceptSignal(){
         signalHandler.handleSignal(1);
@@ -36,6 +58,23 @@ public class SignalHandlerServiceTest {
 
     private List<SignalSpec> generateSampleSignals(){
         List<SignalSpec> signalSpecList = new ArrayList<>();
+        SignalSpec signal1 = new SignalSpec(1L);
+        List<SignalAlgoDetail> signalAlgoDetailList = new ArrayList<>();
+        SignalAlgoDetail signalAlgoDetail1 = new SignalAlgoDetail("setup", SignalAlgoType.GENERIC);
+        SignalAlgoDetail signalAlgoDetail2 = new SignalAlgoDetail("setParam", SignalAlgoType.PARAM);
+        signalAlgoDetail2.setParam1(1);
+        signalAlgoDetail2.setParam2(60);
+
+        SignalAlgoDetail signalAlgoDetail3 = new SignalAlgoDetail("performCalculation", SignalAlgoType.GENERIC);
+        SignalAlgoDetail signalAlgoDetail4 = new SignalAlgoDetail("submitMarket", SignalAlgoType.GENERIC);
+        signalAlgoDetailList.add(signalAlgoDetail1);
+        signalAlgoDetailList.add(signalAlgoDetail2);
+        signalAlgoDetailList.add(signalAlgoDetail3);
+        signalAlgoDetailList.add(signalAlgoDetail4);
+
+        signal1.setAlgoDetailList(signalAlgoDetailList);
+
+        signalSpecList.add(signal1);
         return signalSpecList;
     }
 }
