ca815474e4fc32e9642a6a4b6623e2ed9c281af7
Lakshitha Ranasinghe <lakshitha@techventuras.com>
2023-08-09 20:46:15 +0200

    project structure setup

---
+++ src/main/java/com/db/client/SignalHandler.java
@@ -0,0 +1,5 @@
+package com.db.client;
+
+public interface SignalHandler {
+    void handleSignal(int signal);
+}
---
+++ src/main/java/com/db/controller/SingalController.java
@@ -0,0 +1,7 @@
+package com.db.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SingalController {
+}
