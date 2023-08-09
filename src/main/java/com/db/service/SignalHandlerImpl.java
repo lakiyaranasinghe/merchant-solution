ce14b95b0281cafe136f19b9abdf9dfa802244fa
Lakshitha Ranasinghe <lakshitha@techventuras.com>
2023-08-09 23:06:11 +0200

    should accept signal test with minimum implementation

--- src/main/java/com/db/service/SignalHandlerImpl.java
+++ src/main/java/com/db/service/SignalHandlerImpl.java
@@ -1,10 +1,18 @@
 package com.db.service;
 
+import com.db.client.JiraClient;
 import com.db.lib.SignalHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignalHandlerImpl implements SignalHandler {
+    private final JiraClient jiraClient;
+
+    @Autowired
+    public SignalHandlerImpl(JiraClient jiraClient) {
+        this.jiraClient = jiraClient;
+    }
 
     @Override
     public void handleSignal(int signal) {
--- src/test/java/com/db/test/SignalHandlerServiceTest.java
+++ src/test/java/com/db/test/SignalHandlerServiceTest.java
@@ -2,32 +2,40 @@ package com.db.test;
 
 import com.db.client.JiraClient;
 import com.db.lib.SignalHandler;
-import com.db.model.algo.AlgoGeneric;
-import com.db.model.algo.AlgoParameterized;
+import com.db.model.SignalSpec;
+import com.db.service.SignalHandlerImpl;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SignalHandlerServiceTest {
-    @InjectMocks
     private SignalHandler signalHandler;
 
     @Mock
-    private JiraClient jiraClient;
-
-    @InjectMocks
-    private Map<String, AlgoGeneric> algoGenericMap;
-
-    @InjectMocks
-    private Map<String, AlgoParameterized> algoParameterizedMap;
-
+    private JiraClient jiraClient = mock(JiraClient.class);
+    @BeforeAll
+    public void setUp(){
+        signalHandler = new SignalHandlerImpl(jiraClient);
+        when(jiraClient.fetchSignalSpecs()).thenReturn(generateSampleSignals());
+    }
     @Test
-    public void shouldPerformAlgorithmWhenSignalGiven(){
+    public void shouldAcceptSignal(){
+        signalHandler.handleSignal(1);
+    }
 
+    private List<SignalSpec> generateSampleSignals(){
+        List<SignalSpec> signalSpecList = new ArrayList<>();
+        return signalSpecList;
     }
 }
