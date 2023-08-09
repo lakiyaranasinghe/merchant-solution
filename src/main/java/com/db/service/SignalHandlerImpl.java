b8a5c70d164ec3f574f962ced15eac665da456c5
Lakshitha Ranasinghe <lakshitha@techventuras.com>
2023-08-09 22:46:13 +0200

    add the minimum Signal Handler Implementation

---
+++ src/main/java/com/db/service/SignalHandlerImpl.java
@@ -0,0 +1,13 @@
+package com.db.service;
+
+import com.db.lib.SignalHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SignalHandlerImpl implements SignalHandler {
+
+    @Override
+    public void handleSignal(int signal) {
+
+    }
+}
