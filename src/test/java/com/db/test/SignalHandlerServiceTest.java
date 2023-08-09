cbfb7b89b13b75bd1531ec006ccaba4aa185802b
Lakshitha Ranasinghe <lakshitha@techventuras.com>
2023-08-09 23:45:12 +0200

    improve test data

--- src/test/java/com/db/test/SignalHandlerServiceTest.java
+++ src/test/java/com/db/test/SignalHandlerServiceTest.java
@@ -77,6 +77,7 @@ public class SignalHandlerServiceTest {
 
     private List<SignalSpec> generateSampleSignals(){
         List<SignalSpec> signalSpecList = new ArrayList<>();
+
         SignalSpec signal1 = new SignalSpec(1L);
         List<SignalAlgoDetail> signalAlgoDetailList = new ArrayList<>();
         SignalAlgoDetail signalAlgoDetail1 = new SignalAlgoDetail("setup", SignalAlgoType.GENERIC);
@@ -90,10 +91,24 @@ public class SignalHandlerServiceTest {
         signalAlgoDetailList.add(signalAlgoDetail2);
         signalAlgoDetailList.add(signalAlgoDetail3);
         signalAlgoDetailList.add(signalAlgoDetail4);
-
         signal1.setAlgoDetailList(signalAlgoDetailList);
 
+        SignalSpec signal2 = new SignalSpec(2L);
+        List<SignalAlgoDetail> signal2AlgoDetailList = new ArrayList<>();
+        SignalAlgoDetail signal2AlgoDetail1 = new SignalAlgoDetail("reverse", SignalAlgoType.GENERIC);
+        SignalAlgoDetail signal2AlgoDetail2 = new SignalAlgoDetail("setParam", SignalAlgoType.PARAM);
+        signal2AlgoDetail2.setParam1(1);
+        signal2AlgoDetail2.setParam2(60);
+
+        SignalAlgoDetail signal2AlgoDetail3 = new SignalAlgoDetail("submitMarket", SignalAlgoType.GENERIC);
+        signalAlgoDetailList.add(signal2AlgoDetail1);
+        signalAlgoDetailList.add(signal2AlgoDetail2);
+        signalAlgoDetailList.add(signal2AlgoDetail3);
+
+        signal2.setAlgoDetailList(signal2AlgoDetailList);
+
         signalSpecList.add(signal1);
+        signalSpecList.add(signal2);
         return signalSpecList;
     }
 }
